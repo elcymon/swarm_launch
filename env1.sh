f487d0f13a5153976264ca85c3a7f5148aeeb51f
elcymon <elcymon@gmail.com>
2019-08-28 12:07:03 +0100

    rearrange env sh

--- env1.sh
+++ env1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
+. /opt/ros/indigo/setup.sh
 export ROS_HOSTNAME=10.41.136.187
 export ROS_MASTER_URI=http://10.41.67.2:11311
-. /opt/ros/indigo/setup.sh
 
 exec "$@"
--- envNest.sh
+++ envNest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+. /opt/ros/indigo/setup.sh
 export ROS_HOSTNAME=10.41.67.2
 export ROS_MASTER_URI=http://10.41.67.2:11311
-. /opt/ros/indigo/setup.sh
 
 exec "$@"
