654137740f5b583840931e33c48ad3937b6f9bc6
elcymon <elcymon@gmail.com>
2019-08-28 14:10:14 +0100

    update more environment variables

--- env1.sh
+++ env1.sh
@@ -2,6 +2,8 @@
 
 . /opt/ros/indigo/setup.sh
 export ROS_HOSTNAME=10.41.136.187
+export ROSLAUNCH_SSH_UNKNOWN=1
+export ROS_IP=10.41.136.187
 export ROS_MASTER_URI=http://10.41.67.2:11311
 echo $ROS_MASTER_URI
 exec "$@"
--- envNest.sh
+++ envNest.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 . /opt/ros/indigo/setup.sh
 export ROS_HOSTNAME=10.41.67.2
+export ROSLAUNCH_SSH_UNKNOWN=1
+export ROS_IP=10.41.67.2
 export ROS_MASTER_URI=http://10.41.67.2:11311
 echo $ROS_MASTER_URI
 exec "$@"
