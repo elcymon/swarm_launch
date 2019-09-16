d94e47561525360aa616b168739c62ff2835d584
elcymon <elcymon@gmail.com>
2019-09-16 16:14:21 +0100

    update swarm launch to setup machines

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-export ROS_HOSTNAME=10.42.0.2
+# export ROS_HOSTNAME=$1
 export ROSLAUNCH_SSH_UNKNOWN=1
-export ROS_IP=10.42.0.2
+export ROS_IP=$1
+shift
 export ROS_MASTER_URI=http://10.42.0.1:11311
 
 . /home/turtlebot/catkin_ws/devel/setup.sh
--- envNest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-. /home/turtlebot/catkin_ws/devel/setup.sh
-export ROS_HOSTNAME=10.42.0.1
-export ROSLAUNCH_SSH_UNKNOWN=1
-export ROS_IP=10.42.0.1
-export ROS_MASTER_URI=http://10.42.0.1:11311
-
-exec "$@"
