0593f7e92fbd4ba9e3c21b7e41c1fc8e6f9d9406
elcymon <elcymon@gmail.com>
2019-09-17 08:29:44 +0100

    use eduroam

--- env.sh
+++ env.sh
@@ -3,10 +3,9 @@
 export ROSLAUNCH_SSH_UNKNOWN=1
 export ROS_IP=$1
 shift
-export ROS_MASTER_URI=http://10.42.0.1:11311
+export ROS_MASTER_URI=http://10.41.65.79:11311
 
 . /home/turtlebot/catkin_ws/devel/setup.sh
-
-roslaunch turtlebot_bringup minimal.launch &
+shift
 
 exec "$@"
--- setupLaunch.sh
+++ setupLaunch.sh
@@ -1,3 +1,5 @@
 export ROSLAUNCH_SSH_UNKNOWN=1
-export ROS_HOSTNAME=10.42.0.1
+export ROS_HOSTNAME=10.41.65.79
+roslaunch bringup.launch id:=nest
+
 roslaunch swarm.launch
